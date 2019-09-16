8c6c66eac6f73cb21b0b55bd4ac3bb5311304328
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-09-16 18:13:35 -0500

    met mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +15,9 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+      <Route exact path="/movies/${movie.id}" component={Movie} />
+      
     </div>
   );
 };
